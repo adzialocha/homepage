42c36701238e7865c769775805277ba28768e44f
Andreas Dzialocha <andreas@marmorkuchen.net>
2016-10-26 00:05:15 +0200

    Fix an issue with safari

--- _src/assets/scripts/slider.js
+++ _src/assets/scripts/slider.js
@@ -5,11 +5,21 @@ const SLIDER_RIGHT_SELECTOR = '.slider__right'
 const SLIDER_LEFT_SELECTOR = '.slider__left'
 const SLIDER_SWITCHED_CLASS = 'slider--switched'
 
+const WRAPPER_SELECTOR = '.slider'
+
 export default function initializeSlider() {
   const sliderElem = document.querySelector(SLIDER_SELECTOR)
+  const wrapperElem = document.querySelector(WRAPPER_SELECTOR)
 
   document.querySelector(SLIDER_RIGHT_SELECTOR).addEventListener('mouseenter', () => {
     sliderElem.classList.add(SLIDER_SWITCHED_CLASS)
+
+    /** This might look wierd but it's as wierd as a safari issue where our
+      scrolling div is getting ignored after the user clicked on the gallery.. */
+    wrapperElem.style.marginBottom = '1px'
+    window.setTimeout(() => {
+      wrapperElem.style.marginBottom = 0
+    }, 500)
   })
 
   document.querySelector(SLIDER_LEFT_SELECTOR).addEventListener('mouseenter', () => {
