1ddb8386013c9790c9aee4ada76150b22e73f91e
Andreas Dzialocha <andreas@marmorkuchen.net>
2016-10-26 00:33:13 +0200

    Touch events, fixed body

--- _src/assets/scripts/slider.js
+++ _src/assets/scripts/slider.js
@@ -7,22 +7,30 @@ const SLIDER_SWITCHED_CLASS = 'slider--switched'
 
 const WRAPPER_SELECTOR = '.slider'
 
+const sliderElem = document.querySelector(SLIDER_SELECTOR)
+const wrapperElem = document.querySelector(WRAPPER_SELECTOR)
+
+function showContent() {
+  sliderElem.classList.add(SLIDER_SWITCHED_CLASS)
+
+  /** This might look wierd but it's as wierd as a safari issue where our
+    scrolling div is getting ignored after the user clicked on the gallery.. */
+  wrapperElem.style.marginBottom = '1px'
+  window.setTimeout(() => {
+    wrapperElem.style.marginBottom = 0
+  }, 500)
+}
+
+function showGallery() {
+  sliderElem.classList.remove(SLIDER_SWITCHED_CLASS)
+}
+
+function registerEvents(selector, cb) {
+  document.querySelector(selector).addEventListener('mouseenter', cb)
+  document.querySelector(selector).addEventListener('touchstart', cb)
+}
+
 export default function initializeSlider() {
-  const sliderElem = document.querySelector(SLIDER_SELECTOR)
-  const wrapperElem = document.querySelector(WRAPPER_SELECTOR)
-
-  document.querySelector(SLIDER_RIGHT_SELECTOR).addEventListener('mouseenter', () => {
-    sliderElem.classList.add(SLIDER_SWITCHED_CLASS)
-
-    /** This might look wierd but it's as wierd as a safari issue where our
-      scrolling div is getting ignored after the user clicked on the gallery.. */
-    wrapperElem.style.marginBottom = '1px'
-    window.setTimeout(() => {
-      wrapperElem.style.marginBottom = 0
-    }, 500)
-  })
-
-  document.querySelector(SLIDER_LEFT_SELECTOR).addEventListener('mouseenter', () => {
-    sliderElem.classList.remove(SLIDER_SWITCHED_CLASS)
-  })
+  registerEvents(SLIDER_RIGHT_SELECTOR, showContent)
+  registerEvents(SLIDER_LEFT_SELECTOR, showGallery)
 }
